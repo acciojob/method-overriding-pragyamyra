4473af75bdf8f39c3381ceecfad39a08cad8fd5e
Pragya Kumari <pragyakumari121@gmail.com>
2024-03-21 14:33:24 +0530

    commit message

---
+++ src/main/java/com/driver/A.java
@@ -0,0 +1,8 @@
+package com.driver;
+
+public class A {
+
+    String meth(){
+        return  "Invoking method from class A";
+    }
+}
---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,9 @@
+package com.driver;
+
+public class B extends A{
+
+    String meth(){
+
+        return "Method is overridden in Extended class B";
+     }
+}
