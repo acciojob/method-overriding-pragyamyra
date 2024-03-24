fc42b80b12cbff7d2175072febebf1af5cc97885
Pragya Kumari <pragyakumari121@gmail.com>
2024-03-24 14:16:38 +0530

    commit

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -2,7 +2,15 @@ package com.driver;
 
 public class A {
 
-    String meth(){
+     String meth(){
         return  "Invoking method from class A";
+    }
+}
+
+class B extends A{
+
+    String meth(){
+        //super();
+        return "Method is overridden in Extended class B";
     }
 }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-         A obj = new A();
+         B obj = new B();
        System.out.println( obj.meth());
        //System.out.println(obj.meth());
     }
